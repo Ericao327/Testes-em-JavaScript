86cdb5bbbda517c039e8cfba854e4c51a8dbf996
Eric Lopes de Medeiros <145200474+Ericao327@users.noreply.github.com>
2025-12-02 22:23:41 -0300

    Add files via upload
    Adição de arquivos

---
+++ arrays.js
@@ -0,0 +1,14 @@
+const frutas = ['Uva', 'Banana', 'Kiwi', 'Maça', 'Morango']
+
+console.log(frutas)
+
+console.log('Primeira fruta:', frutas[0])
+console.log('Ultima fruta:', frutas[4])
+
+console.log('Total de frutas:', frutas.length)
+
+frutas.push('Melancia')
+
+console.log('Frutas após adicionar Melancia:', frutas)
+
+console.log('Última fruta:', frutas[frutas.length - 1])
