5d6fb6ad23c88f94fc553db66fbc08b7f39eb2d0
Ericao327 <ericlopesdemedeiros@gmail.com>
2025-12-23 15:02:41 -0300

    Adicionando arquivo

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    nome: 'Ana',
+    idade: 26,
+    temCNH: true
+}
+
+pessoa.sobrenome = 'Paula'
+
+console.log('Nome: ', pessoa.nome)
+console.log('Sobrenome: ', pessoa.sobrenome)
