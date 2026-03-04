13ea52b0e6de1c7aa8eebf988f49fd54e43e8241
Ericao327 <ericlopesdemedeiros@gmail.com>
2026-03-04 10:59:51 -0300

    Descrição da minha alteração

---
+++ teste.js
@@ -0,0 +1,20 @@
+// Arquivo de teste para alterações no Git
+
+console.log("Arquivo de teste criado com sucesso!");
+
+function saudacao(nome) {
+    return `Olá, ${nome}!`;
+}
+
+function soma(a, b) {
+    return a + b;
+}
+
+let usuario = "Eric";
+console.log(saudacao(usuario));
+
+let resultado = soma(10, 5);
+console.log("Resultado da soma:", resultado);
+
+// Simulação de alteração futura
+// Versão 1.0
